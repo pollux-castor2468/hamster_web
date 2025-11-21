ef20900911e56f36e4747b31c6d41d00fc24c73a
yifyu1122 <yifyu1122@gmail.com>
2025-11-21 10:41:29 +0800

    jquery套用

--- js/jquery.js
+++ js/jquery.js
@@ -1,39 +1,39 @@
-$(document).ready(function() {
-    function setOpen(open) {
-        $(".nav-item-toggle").closest(".has-dropdown").toggleClass("open", open);
-        $(".nav-item-toggle").attr("aria-expanded", open);
-    }
+$(function() {
+    function setOpen(element, open) {
+    element.closest(".has-dropdown").toggleClass("open", open);
+    element.attr("aria-expanded", open);
+}
 
-    $(".nav-item-toggle").mouseenter(function () {
-        if (window.innerWidth > 768) setOpen(true);
+    $(".nav-item-toggle").on("mouseenter", function () {
+        if (window.innerWidth > 768) setOpen($(this), true);
     });
 
-    $(".nav-item-toggle").closest(".has-dropdown").mouseleave(function () {
-        if (window.innerWidth > 768) setOpen(false);
+    $(".nav-item-toggle").closest(".has-dropdown").on("mouseleave", function () {
+        if (window.innerWidth > 768) setOpen($(this).find(".nav-item-toggle"), false);
     });
 
-    $(".nav-item-toggle").click(function (e) {
+    $(".nav-item-toggle").on("click", function (e) {
         if (window.innerWidth <= 768) {
             e.preventDefault();
-            $(".nav-item-toggle").closest(".has-dropdown").find(".nav-dropdown").toggleClass('show');
+            $(this).closest(".has-dropdown").find(".nav-dropdown").toggleClass('show');
         }
     });
 
-    $(".nav-item-toggle").closest(".has-dropdown").find(".nav-dropdown").on('click', 'a', function(e) {
-         setTimeout(() => setOpen(false), 120);
+    $(".nav-item-toggle").closest(".has-dropdown").find(".nav-dropdown").on('click', 'a', function() {
+         setTimeout(() => setOpen($(".nav-item-toggle"), false), 120);
     });
 
-    $(document).click(function(e){
+    $(document).on("click", function(e){
         if (!$(".nav-item-toggle").closest(".has-dropdown").is(e.target) && $(".nav-item-toggle").closest(".has-dropdown").has(e.target).length === 0) {
-            setOpen(false);
+            setOpen($(".nav-item-toggle"), false);
         }
     });
 
-    $(document).keydown(function(e){
-        if (e.key === 'Escape') setOpen(false);
+    $(document).on("keydown", function(e){
+        if (e.key === 'Escape') setOpen($(".nav-item-toggle"), false);
     });
 
-    $("#hamburger").click(function(){
+    $("#hamburger").on("click", function(){
         $(this).toggleClass('active');
         $(".nav-link").toggleClass('show');
     });
